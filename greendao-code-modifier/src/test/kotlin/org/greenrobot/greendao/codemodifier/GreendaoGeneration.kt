da8c25603a46e3ec44c77bf7a41cc49a3f5cb1d1
Alexander Dorokhov <alex.dorokhov@greenrobot>
2016-05-30 19:31:26 +0200

    changes in the example generator

--- greendao-code-modifier/src/test/kotlin/org/greenrobot/greendao/codemodifier/GreendaoGeneration.kt
+++ greendao-code-modifier/src/test/kotlin/org/greenrobot/greendao/codemodifier/GreendaoGeneration.kt
@@ -4,8 +4,9 @@ import java.io.File
 
 object GreendaoGeneration {
     @JvmStatic fun main(args: Array<String>) {
-        val files = listOf("Note", "Order", "Customer", "Employee", "EmployeeOrder").map {
-            File("greendao-example/src/main/java/com/example/greendao/${it}.java")
+        val files = listOf("notes/Note", "orders/Order", "orders/Customer",
+            "orders/Employee", "orders/EmployeeOrder").map {
+            File("../greendao-example/src/main/java/com/example/greendao/${it}.java")
         }
 
         val formattingOptions = FormattingOptions().apply {
@@ -15,20 +16,20 @@ object GreendaoGeneration {
         val schemaOptions = SchemaOptions(
             name = "default",
             version = 1,
-            daoPackage = "com.example.greendao",
-            outputDir = File("greendao-example/src/generated/main/java"),
-            testsOutputDir = File("greendao-example/src/generated/test/java")
+            daoPackage = null,
+            outputDir = File("../greendao-example/src/generated/main/java"),
+            testsOutputDir = File("../greendao-example/src/generated/test/java")
         )
 
-        val notesSchemaOptions = schemaOptions.copy(
-            name = "notes",
-            daoPackage = "com.example.greendao.notes",
-            version = 2
-        )
+//        val notesSchemaOptions = schemaOptions.copy(
+//            name = "notes",
+//            daoPackage = "com.example.greendao.notes",
+//            version = 2
+//        )
 
         Greendao3Generator(formattingOptions).run(files, mapOf(
-            "default" to schemaOptions,
-            "notes" to notesSchemaOptions
+            "default" to schemaOptions
+//            "notes" to notesSchemaOptions
         ))
     }
 }
