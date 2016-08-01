9533a40d8e50810e4a59d190efcf2e2c0a85ccbb
Uwe Trottmann <uwe@greenrobot.de>
2016-08-01 13:40:55 +0200

    Add initial generator test (constructor generation).
    - Currently constructors created in wrong order (more-args first). Not easily fixable as the class transformer decides based on the initially parsed class where to put things and does not take into account changes it has already made.

---
+++ greendao-code-modifier/src/test/kotlin/org/greenrobot/greendao/codemodifier/Greendao3GeneratorTest.kt
@@ -0,0 +1,51 @@
+package org.greenrobot.greendao.codemodifier
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+
+class Greendao3GeneratorTest {
+
+    private val samplesDirectory: File = File("test-files/")
+
+    private val testDirectory: File = File("build/tmp/greendaoTest/")
+
+    private val formattingOptions: FormattingOptions = FormattingOptions().apply {
+        this.lineWidth = 120
+    }
+
+    private val schemaOptions: SchemaOptions = SchemaOptions(
+            name = "default",
+            version = 1,
+            daoPackage = null,
+            outputDir = File(testDirectory, "main/java"),
+            testsOutputDir = File(testDirectory, "test/java")
+    )
+
+    @Before
+    fun ensureEmptyTestDirectory() {
+        testDirectory.deleteRecursively()
+        assert(!testDirectory.isDirectory || testDirectory.list().isEmpty())
+    }
+
+    @Test
+    fun testCreateConstructor() {
+        generateAndAssertFile("CreateConstructor")
+    }
+
+    fun generateAndAssertFile(baseFileName : String) {
+        // copy the input file to the test directory
+        val inputFile = File(samplesDirectory, baseFileName + "Input.java")
+        val targetFile = inputFile.copyTo(File(testDirectory, baseFileName + "Actual.java"), true)
+
+        // run the generator over the file
+        Greendao3Generator(formattingOptions).run(listOf(targetFile), mapOf("default" to schemaOptions))
+
+        // check if the modified file matches the expected output file
+        val actualSource = targetFile.readText(charset("UTF-8"))
+        val expectedSource = File(samplesDirectory, baseFileName + "Expected.java").readText(charset("UTF-8"))
+        assertEquals(expectedSource, actualSource)
+    }
+
+}
---
+++ greendao-code-modifier/test-files/CreateConstructorExpected.java
@@ -0,0 +1,30 @@
+package org.greenrobot.greendao.example;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+
+@Entity
+public class Note {
+
+    @Id
+    private Long id;
+
+    @Generated(hash = 1390446558)
+    public Note(Long id) {
+        this.id = id;
+    }
+
+    @Generated(hash = 1272611929)
+    public Note() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
