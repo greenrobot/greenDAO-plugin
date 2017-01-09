28234b0b96f7407b914c416931994939261823e8
Uwe Trottmann <uwe@greenrobot.de>
2017-01-09 12:08:14 +0100

    Add tests for some other ignored parser problems.

--- greendao-code-modifier/src/test/kotlin/org/greenrobot/greendao/codemodifier/VisitorIgnoredProblemsTest.kt
+++ greendao-code-modifier/src/test/kotlin/org/greenrobot/greendao/codemodifier/VisitorIgnoredProblemsTest.kt
@@ -2,6 +2,10 @@ package org.greenrobot.greendao.codemodifier
 
 import org.junit.Test
 
+/**
+ * Problems the AST parse may have when looking at entity source code but which have no impact on the plugin working
+ * correctly. See [EntityClassParser.ignoredProblemIds].
+ */
 class VisitorIgnoredProblemsTest : VisitorTestBase() {
 
     @Test
@@ -10,15 +14,78 @@ class VisitorIgnoredProblemsTest : VisitorTestBase() {
                 //language=java
                 """
         import org.greenrobot.greendao.annotation.Entity;
+        import org.jetbrains.annotations.NotNull;
 
         @Entity
         class Foobar implements Comparable<SomeType> {
 
-                @Override
-                public int compareTo(SomeType another) {
-                    return 0;
-                }
+            // Comparable implementation of unresolvable type
+            @Override
+            public int compareTo(@NotNull SomeType another) {
+                return 0;
+            }
 
+        }
+        """
+        )
+    }
+
+    @Test
+    fun missingTypeInMethod() {
+        visit(
+                //language=java
+                """
+        import org.greenrobot.greendao.annotation.Entity;
+
+        @Entity
+        class Foobar {
+
+            public void doSomething() {
+                // unresolvable type a method refers to
+                SomeType some = getSomeType();
+            }
+
+            public SomeType getSomeType() {
+                return null;
+            }
+
+        }
+        """
+        )
+    }
+
+    @Test
+    fun undefinedName() {
+        visit(
+                //language=java
+                """
+        import org.greenrobot.greendao.annotation.Entity;
+        import android.text.TextUtils;
+
+        @Entity
+        class Foobar {
+
+            public void doSomething() {
+                // unresolvable external class reference, like TextUtils
+                TextUtils.isEmpty("");
+            }
+
+        }
+        """
+        )
+    }
+
+    @Test
+    fun unsupportedSuppressWarnings() {
+        visit(
+                //language=java
+                """
+        import org.greenrobot.greendao.annotation.Entity;
+
+        // SuppressWarnings tokens supported by IntelliJ, but not Eclipse
+        @SuppressWarnings("WeakerAccess")
+        @Entity
+        class Foobar {
         }
         """
         )
