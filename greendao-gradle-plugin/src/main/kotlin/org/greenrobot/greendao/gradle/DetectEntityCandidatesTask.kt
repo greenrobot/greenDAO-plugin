2189b02e3591e4d2d12930c7e5654fc25ea7a310
Uwe Trottmann <uwe@greenrobot.de>
2023-06-26 16:15:22 +0200

    Migrate from IncrementalTaskInputs to InputChanges for Gradle 8 compat.

--- greendao-gradle-plugin/src/main/kotlin/org/greenrobot/greendao/gradle/DetectEntityCandidatesTask.kt
+++ greendao-gradle-plugin/src/main/kotlin/org/greenrobot/greendao/gradle/DetectEntityCandidatesTask.kt
@@ -6,8 +6,8 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.incremental.IncrementalTaskInputs
-import org.gradle.api.tasks.incremental.InputFileDetails
+import org.gradle.work.ChangeType
+import org.gradle.work.InputChanges
 import java.io.File
 import java.nio.charset.Charset
 
@@ -39,13 +39,13 @@ open class DetectEntityCandidatesTask : DefaultTask() {
     private val token = "org.greenrobot.greendao.annotation".toCharArray()
 
     @TaskAction
-    fun execute(inputs: IncrementalTaskInputs) {
+    fun execute(inputChanges: InputChanges) {
         val candidatesFile = this.candidatesListFile ?: throw IllegalStateException("candidates should be defined")
         val sourceFiles = this.sourceFiles ?: throw IllegalStateException("source files should be defined")
         val charset = charset(charset)
 
-        if (inputs.isIncremental && candidatesFile.exists()) {
-            processIncremental(inputs, candidatesFile, charset)
+        if (inputChanges.isIncremental && candidatesFile.exists()) {
+            processIncremental(inputChanges, sourceFiles, candidatesFile, charset)
         } else {
             processComplete(candidatesFile, sourceFiles, charset)
         }
@@ -60,7 +60,12 @@ open class DetectEntityCandidatesTask : DefaultTask() {
             .let { writeCandidates(it, candidatesFile) }
     }
 
-    private fun processIncremental(inputs: IncrementalTaskInputs, candidatesFile: File, charset: Charset) {
+    private fun processIncremental(
+        inputChanges: InputChanges,
+        sourceFiles: FileCollection,
+        candidatesFile: File,
+        charset: Charset
+    ) {
         val buffer = CharArray(DEFAULT_BUFFER_SIZE)
 
         // read candidates, skipping first line with timestamp
@@ -69,22 +74,22 @@ open class DetectEntityCandidatesTask : DefaultTask() {
         // will be set to true if any previously known candidate was modified
         var modifiedExisting = false
 
-        // check each modified file and add/remove candidates
-        inputs.outOfDate { change: InputFileDetails ->
-            val file = change.file
-            if (file.isFile) {
-                if (Util.containsIgnoreSpaces(file, token, buffer, charset)) {
-                    val listChanged = newCandidates.add(file.path)
-                    modifiedExisting = !listChanged or modifiedExisting
-                } else {
-                    newCandidates.remove(file.path)
+        inputChanges.getFileChanges(sourceFiles).forEach { change ->
+            if (change.changeType == ChangeType.ADDED || change.changeType == ChangeType.MODIFIED) {
+                // check each modified file and add/remove candidates
+                val file = change.file
+                if (file.isFile) {
+                    if (Util.containsIgnoreSpaces(file, token, buffer, charset)) {
+                        val listChanged = newCandidates.add(file.path)
+                        modifiedExisting = !listChanged or modifiedExisting
+                    } else {
+                        newCandidates.remove(file.path)
+                    }
                 }
+            } else if (change.changeType == ChangeType.REMOVED) {
+                // removed candidates for removed files
+                newCandidates.remove(change.file.path)
             }
-        }
-
-        // removed candidates for removed files
-        inputs.removed { change: InputFileDetails ->
-            newCandidates.remove(change.file.path)
         }
 
         // only if list is changed or previously added candidate is changed
